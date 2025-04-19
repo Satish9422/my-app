7299c4ca559f4fde0b1ea6c04b672c96dd379bc5
satish <satish@gmail.com>
2025-04-19 17:26:47 +0530

    green deployment

--- app/app.cjs
+++ app/app.cjs
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
 
-app.get("/", (req, res) => res.send("Hello This is Demo App"));
+app.get("/", (req, res) => res.send("Hello This is Green Deployment"));
 
 // app.get("/", (req, res) => res.send("Hello "));
 app.get("/health", (req, res) => {
